e27945a08b13ffdedf20d351666de3d151674989
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2021-04-05 20:34:08 -0300

    profile styles

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 import Background from '../../components/background/Background'
 import StudentHeader from '../../components/studentHeader/StudentHeader';
@@ -31,27 +31,51 @@ const Profile = ({ navigation }) => {
       ) : (
         <View>
           <StudentHeader title="Perfil" openDrawer={navigation.openDrawer}/>
-          <View>
+          <View style={styles.imageContainer}>
             <Image
               source={userPicture}
             />
           </View>
-          <View>
-            <Text>Nome: {profile.name}</Text>
-            <Text>Email: {profile.email}</Text>
-            <Text>CPF: {profile.cpf}</Text>
-            <Text>Data de nascimento: {formatedDate}</Text>
-            <Text>Curso: {profile.course}</Text>
-            <Text>RA: {profile.code}</Text>
-            <Text>Periodo: {profile.period}</Text>
-            <Text>PP: {profile.progress}</Text>
-            <Text>PR: {profile.averageGrade}</Text>
-            <Text>Unidade: {profile.unit}</Text>
+          <View style={styles.infoContainer}>
+            <Text style={styles.infos}>Nome: {profile.name}</Text>
+            <Text style={styles.infos}>Email: {profile.email}</Text>
+            <Text style={styles.infos}>CPF: {profile.cpf}</Text>
+            <Text style={styles.infos}>RA: {profile.code}</Text>
+            <Text style={styles.infos}>Data de nascimento: {formatedDate}</Text>
+            <Text style={styles.infos}>{profile.unit}</Text>
+            <Text style={styles.infos}>Curso: {profile.course}</Text>
+            <View style={styles.infoWrapper}>
+              <Text style={styles.infos}>Periodo: {profile.period}</Text>
+              <Text style={styles.infos}>PP: {profile.progress}</Text>
+              <Text style={styles.infos}>PR: {profile.averageGrade}</Text>
+            </View>
           </View>
         </View>
       )}
     </Background>
   );
 }
+
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 50,
+    borderBottomWidth: 8,
+    borderBottomColor: 'rgba(35, 49, 170, 0.1)'
+  },
+  infoContainer: {
+    marginTop: 20
+  },
+  infoWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  infos: {
+    marginTop: 10,
+    fontSize: 16
+  }
+})
 
 export default Profile;
