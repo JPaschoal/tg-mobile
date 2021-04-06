5325943bee11e34ed9d21c5178cc1728c95efcb8
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2021-04-05 21:40:25 -0300

    getting grades

--- src/screens/grade/Grade.js
+++ src/screens/grade/Grade.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Circle } from 'react-native-animated-spinkit';
+import DropDownPicker from 'react-native-dropdown-picker';
 
+import Background from '../../components/background/Background';
+import StudentHeader from '../../components/studentHeader/StudentHeader';
+import api from '../../services/api';
 
-const Grade = () => {
+const Grade = ({ navigation }) => {
+  const [grades, setGrades] = useState()
+  const [loading, setLoading] = useState(true)
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    api.get('grades')
+    .then((response) => {
+      setGrades(response.data)
+      setLoading(false)
+
+      const itemsList = grades.map((element) => {
+        return {label: element.discipline.name, value: element.discipline.code}
+      })
+
+      setItems(itemsList)
+    })
+  }, [])
+
   return (
-  <View style={styles.container}>
-    <Text>
-      Notas
-    </Text>
-  </View>);
+    <Background>
+      <StudentHeader title="Notas" openDrawer={navigation.openDrawer} />
+      {loading ? (
+        <Circle
+          size={80}
+          color='#012480'
+          style={styles.loading}
+        />
+      ) : (
+        <View>
+          <DropDownPicker
+            items={items}
+            containerStyle={{height: 40}}
+            style={{backgroundColor: '#fafafa'}}
+            itemStyle={{
+                justifyContent: 'flex-start'
+            }}
+            dropDownStyle={{backgroundColor: '#fafafa'}}
+            placeholder="Selecione uma disciplina"
+            onChangeItem={(item) => {console.warn(item.value)}}
+          />
+        </View>
+      )}
+    </Background>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
 
 export default Grade;
