5f0cbfe1f4f0bc7d7340e4b750aad8b717a4855f
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2021-04-10 15:53:46 -0300

    screens structure changes

---
+++ src/screens/student/schedules/Schedule.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Schedules = () => {
+  return (
+    <View>
+      <Text>
+        Horario
+      </Text>
+    </View>
+  );
+}
+
+export default Schedules;
