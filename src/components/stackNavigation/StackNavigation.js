4a9fb83d8f023ea39ed6609540f6da0c6ad9813f
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2021-04-19 20:37:43 -0300

    adding new screen

--- src/components/stackNavigation/StackNavigation.js
+++ src/components/stackNavigation/StackNavigation.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import NotebooksList from '../../screens/notebooks/NotebooksList/NotebooksList'
 import NotesList from '../../screens/notes/notesList/notesList';
 import { NotebookProvider } from '../../contexts/Notebook';
+import Note from '../../screens/notes/note/Note'
 
 const Stack = createStackNavigator()
 
@@ -16,6 +17,7 @@ const StackNavigation = () => {
       >
         <Stack.Screen name="Cadernos" component={NotebooksList} />
         <Stack.Screen name="Anotações" component={NotesList} />
+        <Stack.Screen name="Anotação" component={Note} />
       </Stack.Navigator>
     </NotebookProvider>
   );
