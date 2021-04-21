c1bc0cf65a306ab75c53e46b78cd707921c2a45b
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2021-04-21 16:03:26 -0300

    creating note context

---
+++ src/contexts/Note.js
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from 'react';
+
+export const NoteContext = createContext([])
+
+export const NoteProvider = ({ children }) => {
+  const [selectedNote, setSelectedNote] = useState()
+  
+  return(
+    <NoteContext.Provider value={[selectedNote, setSelectedNote]}>
+      {children}
+    </NoteContext.Provider>
+  );
+}
